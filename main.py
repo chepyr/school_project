3df8230f8d560fa9051ffdc181bf78d5db0c5074
Anya <anna.artamono@gmail.com>
2022-02-16 07:41:36 +0300

    Добавлена пара страниц про экзопланеты

--- main.py
+++ main.py
@@ -3,15 +3,29 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
+@app.route('/')
+def main():
+    return render_template("main.html", which_button="btn-r")
+
+
 @app.route('/<button>')
-def hello(button):
+def main_btn(button):
     return render_template("main.html", which_button=button)
 
 
 @app.route('/Barnards-star-b/<button>')
 def Barnards_star_b(button):
-    print(button)
     return render_template("Barnards_star_b.html", which_button=button)
+
+
+@app.route('/TOI-700-d/<button>')
+def TOI_700_d(button):
+    return render_template("toi_700_d.html", which_button=button)
+
+
+@app.route('/proxima-b/<button>')
+def Proxima_b(button):
+    return render_template("proxima-b.html", which_button=button)
 
 
 if __name__ == '__main__':
