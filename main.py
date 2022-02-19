a4b4bcdc55a9bc3bc6981de5abedf97531b19107
Anya <anna.artamono@gmail.com>
2022-02-19 19:58:16 +0300

    изменены настройки порта

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -35,5 +37,5 @@ def Proxima_b(button):
 
 
 if __name__ == '__main__':
-    app.debug = True
-    app.run(host='0.0.0.0')
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
